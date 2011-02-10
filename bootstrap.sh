13aa6d8b2b56d1146f784a27c8440fcf854e139f
russo <russo>
2011-02-10 20:01:18 +0000

    Change version string in AM_INIT_AUTOMAKE

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+echo -n '1) aclocal...'
 aclocal
+echo 'done.'
+echo -n '2) autoheader...'
 autoheader
+echo 'done.'
+echo -n '3) autoconf...'
 autoconf
+echo 'done.'
+echo -n '4) automake...'
 automake -a -c
+echo 'done.'
