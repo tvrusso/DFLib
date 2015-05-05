8c9abda69809e4dfecb137de169ef549be10ea18
Tom Russo <russo@bogodyn.org>
2015-05-05 11:23:02 -0600

    Modify DFLib.i so it prints more useful output for exceptions (I had a lot of errors due to not getting the "Clone" function written correctly, and all I got for debugging without this change was a notice that an exception happened).
    Add actual DFLib fix computation tests to "pyPointTest.py".
    
    These tests require the "pyproj" package, http://jswhit.github.io/pyproj/
    
    The test performed by pyPointTest is basically the same as a run of
    testlsDFfix with the transmitter at 106d35'W 34d55'N, and one particular
    randomization of bearings.  Confirms that this python code is producing
    the same fixes as the C++ code does.
    
    The python is a little fragile, but it does work, and could probably
    be used as is.  One could refine the definitions of the pyPoint and
    pyReport classes to do the same sort of "user coordinate <-> XY"
    coordinate stuff that the fancier DFLib classes do.  Or just use them
    as is and do the coordinate conversion yourself.  The main advantage
    of doing the coordinate conversion in the "Point" class would be to
    make sure the FCA standard deviations are computed in user coordinate
    units rather than XY units, as the deviations are pretty useless in XY
    coordinates.

--- Python/DFLib.i
+++ Python/DFLib.i
@@ -1,4 +1,13 @@
 %module(directors="1") DFLib
+%feature("director:except") {
+  if( $error != NULL ) {
+    PyObject *ptype, *pvalue, *ptraceback;
+    PyErr_Fetch( &ptype, &pvalue, &ptraceback );
+    PyErr_Restore( ptype, pvalue, ptraceback );
+    PyErr_Print();
+    Py_Exit(1);
+  }
+ } 
 %feature("director");
 %include "std_string.i"
 %include "std_vector.i"
--- Python/pyPointTest.py
+++ Python/pyPointTest.py
@@ -1,6 +1,7 @@
 import DFLib
 import copy
 import math
+from pyproj import Proj
 
 class pyPoint(DFLib.Point):
    def __init__(self):
@@ -15,8 +16,8 @@ class pyPoint(DFLib.Point):
      return self.myXY
   # This is NOT really a "clone" operation, but it serves the purpose enough
   # for DFLib
-   def clone(self):
-     foo=pyPoint()
+   def Clone(self):
+     foo=pyPoint().__disown__()
      foo.setXY(self.getXY())
      return foo
    def getUserCoords(self):
@@ -53,35 +54,70 @@ class pyReport(DFLib.Report):
       return self.sigma
    def getSigma(self):
       return self.getBearingStandardDeviationRadians()*180.0/math.pi
-   
+
+#Create 3 reports.  These correspond exactly to the "receivers3" points
+#in the main DFLib directory.
+# The bearings are the product of a single run of testlsDFfix, randomizing
+# around the true bearings to a transmitter at 106d35'W 34d55'N
+# according to the sigmas associated with the transmitters
+#
+mercProj = Proj('+proj=merc +datum=WGS84 +lat_ts=0')   
 v=DFLib.vectord(2)
-v[0]=1.0
-v[1]=2.5
-p=pyPoint()
-p.setXY(v)
-p2=p.clone()
-v[0]=3.0
-v[1]=4.5
-p2.setXY(v)
-
-v2=p.getXY()
-print 'v2: %f %f'%(v2[0],v2[1])
-v3=p2.getXY()
-print 'v3: %f %f'%(v3[0],v3[1])
-
+v[0],v[1]=mercProj(-(106+38./60.+15.4/3600.),34+56./60.+48.7/3600)
 r1=pyReport("r1",True)
-r1.setReceiverLocation(v2)
-r1.setBearing(175.0)
-r1.setSigma(27.0)
+r1.setReceiverLocation(v)
+r1.setBearing(123.866)
+r1.setSigma(1.0)
 r1p=r1.getReceiverLocation()
 
+v[0],v[1]=mercProj(-(106+18./60.+17.0/3600.),34+58./60.+12.5/3600)
 r2=pyReport("r2",True)
-r2.setReceiverLocation(v3)
-r2.setBearing(185.0)
-r2.setSigma(18.0)
+r2.setReceiverLocation(v)
+r2.setBearing(-104.265)
+r2.setSigma(3.0)
 r2p=r2.getReceiverLocation()
+
+v[0],v[1]=mercProj(-(106+28./60.+53.8/3600.),35+10./60.+33.9/3600)
+r3=pyReport("r3",True)
+r3.setReceiverLocation(v)
+r3.setBearing(-160.354)
+r3.setSigma(2.0)
+r3p=r3.getReceiverLocation()
 
 print 'r1: (x,y)=(%f,%f)  bearing=%f sigma=%f'%(r1p[0],r1p[1],r1.getBearing(),r1.getSigma())
 print 'r1: (x,y)=(%f,%f)  bearing(rad)=%f sigma(rad)=%f'%(r1p[0],r1p[1],r1.getReportBearingRadians(),r1.getBearingStandardDeviationRadians())
 print 'r2: (x,y)=(%f,%f)  bearing=%f sigma=%f'%(r2p[0],r2p[1],r2.getBearing(),r2.getSigma())
 print 'r2: (x,y)=(%f,%f)  bearing(rad)=%f sigma(rad)=%f'%(r2p[0],r2p[1],r2.getReportBearingRadians(),r2.getBearingStandardDeviationRadians())
+
+print 'r3: (x,y)=(%f,%f)  bearing=%f sigma=%f'%(r3p[0],r3p[1],r3.getBearing(),r3.getSigma())
+print 'r3: (x,y)=(%f,%f)  bearing(rad)=%f sigma(rad)=%f'%(r3p[0],r3p[1],r3.getReportBearingRadians(),r3.getBearingStandardDeviationRadians())
+
+rc=DFLib.ReportCollection()
+rc.addReport(r1)
+rc.addReport(r2)
+rc.addReport(r3)
+
+LSfix=pyPoint()
+rc.computeLeastSquaresFix(LSfix)
+
+LSv=LSfix.getXY()
+print 'ls fix: x=%f y=%f'%(LSv[0],LSv[1])
+print 'ls fix(LL): x=%f y=%f'%mercProj(LSv[0],LSv[1],inverse=True)
+
+FCAfix=pyPoint()
+FCA_stddev=DFLib.vectord(2)
+rc.computeFixCutAverage(FCAfix,FCA_stddev)
+   
+FCAv=FCAfix.getXY()
+
+print 'FCA fix: x=%f y=%f'%(FCAv[0],FCAv[1])
+print 'FCA fix(LL): x=%f y=%f'%mercProj(FCAv[0],FCAv[1],inverse=True)
+print 'FCA stddevs(merc) x=%f y=%f'%(FCA_stddev[0],FCA_stddev[1])
+
+MLfix = LSfix.Clone()
+rc.aggressiveComputeMLFix(MLfix)
+
+
+MLv=MLfix.getXY()
+print 'ML fix: x=%f y=%f'%(MLv[0],MLv[1])
+print 'ML fix(LL): x=%f y=%f'%mercProj(MLv[0],MLv[1],inverse=True)
